3dd1992b9079a0f5b5e3fd444475493c125031fe
Colin Bakker <acojaba@gmail.com>
2024-04-04 21:06:49 -0500

    Created files in each folder to create the folders in github

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -2,7 +2,8 @@
 export PATH_TO_FX=/home/colin/Documents/libraries/javafx-sdk-22/lib
 
 #compile, add src/controller*.java and src/model*.java when files added
-javac --module-path $PATH_TO_FX --add-modules javafx.controls -d bin src/view/*.java
+javac --module-path $PATH_TO_FX --add-modules javafx.controls -d bin src/view/*.java src/controller/*.java src/model/*.java
+
 
 #run
 java --module-path $PATH_TO_FX --add-modules javafx.controls -classpath bin view.App
